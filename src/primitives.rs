097e46369aeab9e4bc8b1792ff0e22749476d65f
0xSherlokMo <mohamed.sorour@halan.com>
2023-04-27 04:04:58 +0200

    primitives

---
+++ src/primitives.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut x = 12i128;
+    println!("`x` is {}", x);
+    x = 13;
+    println!("`x` is {}", x);
+    let x = true; // WHY WOULD I DO THAT????
+    println!("`x` is {}", x);
+}
